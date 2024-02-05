a6d7354a1894f2fe37a48442ee391c4d5829173e
Herik25 <128887958+Herik25@users.noreply.github.com>
2024-02-05 18:54:39 +0530

    responsive : myOrders and myAccount page

--- src/pages/MyAccount.jsx
+++ src/pages/MyAccount.jsx
@@ -7,37 +7,38 @@ function MyAccount() {
   return (
     <div className=" font-Montserrat">
       <Navbar />
-      <div className=" mx-44 mb-20">
+      <div className=" mx-2 md:mx-8 lg:mx-32 xl:mx-48 mb-20">
         <h1 className="text-3xl font-bold tracking-tight text-gray-900 pb-2 mt-24">
           My Account
         </h1>
         <div className="h-[2px] w-[100px] bg-yellow-400"></div>
       </div>
-      <div className=" mx-44 flex justify-around border-b-[1px] border-b-[#979797] border-opacity-30 pb-4">
-        <Link to="/myorders">
-          <div className="  p-3 border-r-[1px] border-r-[#979797] border-opacity-30 max-w-[180px] cursor-pointer" >
-            <div className=" flex items-center">
-              <span className=" font-bold text-base">My Orders</span>
-              <BsChevronRight className=" text-[#737373] ml-4" />
-            </div>
-            <span className=" text-xs text-[#181818] opacity-70">
-              View, modify and track orders
-            </span>
+      <div className="mx-2 grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4  xl:grid-cols-5 justify-around border-b-[1px] border-b-[#979797] border-opacity-30 pb-4 md:mx-8 lg:mx-32 xl:mx-48">
+        <Link
+          to="/myorders"
+          className="p-3 border-r-[1px] border-r-[#979797] border-opacity-30 cursor-pointer"
+        >
+          <div className=" flex items-center">
+            <span className=" font-bold text-base">My Orders</span>
+            <BsChevronRight className=" text-[#737373] ml-4" />
           </div>
+          <span className=" text-xs text-[#181818] opacity-70  max-w-[180px]">
+            View, modify and track orders
+          </span>
         </Link>
-        <div className=" p-3 border-r-[1px] border-r-[#979797] border-opacity-30 max-w-[200px] cursor-not-allowed">
+        <div className=" p-3 border-r-[1px] border-r-[#979797] border-opacity-30 cursor-not-allowed">
           <div className=" flex items-center">
             <span className=" font-bold text-base">My Payments</span>
             <BsChevronRight className=" text-[#737373] ml-4" />
           </div>
-          <span className=" text-xs text-[#181818] opacity-70">
+          <span className=" text-xs text-[#181818] opacity-70 max-w-[200px]">
             View and modify payment methods
           </span>
         </div>
-        <div className=" p-3 border-r-[1px] border-r-[#979797] border-opacity-30 max-w-[200px] cursor-not-allowed">
+        <div className=" p-3 border-r-[1px] border-r-[#979797] border-opacity-30  cursor-not-allowed">
           <div className=" flex items-center">
             <span className=" font-bold text-base">My Wallet</span>
-            <span className=" font-bold text-xs text-[#20a437] ml-2">
+            <span className=" font-bold text-xs text-[#20a437] ml-2 max-w-[180px]">
               Rs. 0
             </span>
             <BsChevronRight className=" text-[#737373] ml-2" />
@@ -46,30 +47,31 @@ function MyAccount() {
             Bewakoof Wallet History and redeemed gift cards
           </span>
         </div>
-        <Link to='/myaddresses'>
-        <div className=" p-3 border-r-[1px] border-r-[#979797] border-opacity-30 max-w-[200px] cursor-pointer">
+        <Link
+          to="/myaddresses"
+          className=" p-3 border-r-[1px] border-r-[#979797] border-opacity-30 cursor-pointer"
+        >
           <div className=" flex items-center">
             <span className=" font-bold text-base">My Addresses</span>
             <BsChevronRight className=" text-[#737373] ml-4" />
           </div>
-          <span className=" text-xs text-[#181818] opacity-70">
+          <span className=" text-xs text-[#181818] opacity-70 max-w-[200px] ">
             Edit, add or remove addresses
           </span>
-        </div>
         </Link>
-        <Link to='/myprofile'>
-        <div className=" p-3 border-opacity-30 max-w-[200px] cursor-pointer">
-          <div className=" flex items-center">
-            <span className=" font-bold text-base">My profile</span>
-            <BsChevronRight className=" text-[#737373] ml-4" />
+        <Link to="/myprofile">
+          <div className=" p-3 border-opacity-30 cursor-pointer">
+            <div className=" flex items-center">
+              <span className=" font-bold text-base">My profile</span>
+              <BsChevronRight className=" text-[#737373] ml-4" />
+            </div>
+            <span className=" text-xs text-[#181818] opacity-70  max-w-[200px]">
+              Edit personal info, change password
+            </span>
           </div>
-          <span className=" text-xs text-[#181818] opacity-70">
-            Edit personal info, change password
-          </span>
-        </div>
         </Link>
       </div>
-      <div className=" flex items-center mx-44 py-28">
+      <div className=" flex items-center py-28 mx-2 md:mx-8 lg:mx-32 xl:mx-48">
         <div className="max-w-[350px]">
           <div className=" text-xl font-bold opacity-90 text-[#747474]">
             Buy something to get personalised recommendations.
